1b6527f68c52d590785e318acedb3edbb2cc1916
Brandon Barrett <bbarrett148@gmail.com>
2026-09-16 02:21:34 -0400

    Added a variable to find the validation digit outside side of the find_UPC function, and added the variable as an argument to the function

--- Lab1_eomkid-1.py
+++ Lab1_eomkid-1.py
@@ -12,11 +12,14 @@ while len(user_upc) != 12 or not user_upc.isdigit():
     user_upc = input(
         "\nThat input is invalid. \nPlease enter a 12 numerical digit UPC:\n").strip()
 
+upc_validation_digit = user_upc[11]
 
-def find_UPC(upc_to_validate):
+
+def find_UPC(upc_to_validate, validation_digit=upc_validation_digit):
     odd_positioned_upc_digits = [upc_to_validate[0:11:2]]
     even_positioned_upc_digits = [upc_to_validate[1:11:2]]
-    print(f"{odd_positioned_upc_digits} {even_positioned_upc_digits}")
+    print(f"""Odd Positioned:{odd_positioned_upc_digits} \nEven Positioned:{even_positioned_upc_digits}
+Validation Digit Type and value:{type(validation_digit)} {validation_digit}""")
     return
 
 
