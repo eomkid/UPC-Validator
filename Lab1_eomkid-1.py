6c4db168153d1f18695d875803fb0244e6817a86
Brandon Barrett <bbarrett148@gmail.com>
2026-09-16 01:27:44 -0400

    Simple input validation for User UPC input now includes both a numerical check and a count for the length of input

--- Lab1_eomkid-1.py
+++ Lab1_eomkid-1.py
@@ -4,7 +4,11 @@ Purpose: To validate 12 digit UPC's
 Date of Creation September 16,2026
 
 """
+# 123456789101 number for testing purposes
+
 user_upc = input("Please the UPC you would like validated:\n")
-while len(user_upc) != 12:
+
+while len(user_upc) != 12 or not user_upc.isdigit():
     user_upc = input("Please enter a 12 numerical digit UPC:\n")
-print(f"{user_upc}")
+
+print(f"{user_upc} {type(user_upc)}")
