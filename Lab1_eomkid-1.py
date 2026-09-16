ced4595a9be44bf822d79a68c6d04982573da642
Brandon Barrett <bbarrett148@gmail.com>
2026-09-16 02:00:32 -0400

    Added a basic function that currently only extracts digits from the upc in the even and odd positions

--- Lab1_eomkid-1.py
+++ Lab1_eomkid-1.py
@@ -4,12 +4,20 @@ Purpose: To validate 12 digit UPC's
 Date of Creation September 16,2026
 
 """
-# 123456789101 number for testing purposes
+# 036000291452 valid UPC for testing purposes
 
-user_upc = input("Please the UPC you would like validated:\n")
+user_upc = input("Please the 12-digit UPC you would like validated:\n").strip()
 
 while len(user_upc) != 12 or not user_upc.isdigit():
     user_upc = input(
-        "\nThat input is invalid. \nPlease enter a 12 numerical digit UPC:\n")
+        "\nThat input is invalid. \nPlease enter a 12 numerical digit UPC:\n").strip()
 
-print(f"{user_upc} {type(user_upc)}")
+
+def find_UPC(upc_to_validate):
+    odd_upc_digits = [upc_to_validate[0:13:2]]
+    even_upc_digits = [upc_to_validate[1:13:2]]
+    print(f"{odd_upc_digits} {even_upc_digits}")
+    return
+
+
+find_UPC(user_upc)
