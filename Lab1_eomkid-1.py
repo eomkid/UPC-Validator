e35f39058f1df5eb2a6c9f33a63315e5b9ee081f
Brandon Barrett <bbarrett148@gmail.com>
2026-09-16 01:03:56 -0400

    Testing of simple input validation, currently only counting length of input

--- Lab1_eomkid-1.py
+++ Lab1_eomkid-1.py
@@ -4,3 +4,7 @@ Purpose: To validate 12 digit UPC's
 Date of Creation September 16,2026
 
 """
+user_upc = input("Please the UPC you would like validated:\n")
+while len(user_upc) != 12:
+    user_upc = input("Please enter a 12 numerical digit UPC:\n")
+print(f"{user_upc}")
