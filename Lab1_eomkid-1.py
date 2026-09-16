cdccdf3b75e94e0f1fe884e6731012cb51ad50a0
Brandon Barrett <bbarrett148@gmail.com>
2026-09-16 01:31:46 -0400

    Adjusted while loop to inform user that their input is invalid and they need to enter a new UPC

--- Lab1_eomkid-1.py
+++ Lab1_eomkid-1.py
@@ -9,6 +9,7 @@ Date of Creation September 16,2026
 user_upc = input("Please the UPC you would like validated:\n")
 
 while len(user_upc) != 12 or not user_upc.isdigit():
-    user_upc = input("Please enter a 12 numerical digit UPC:\n")
+    user_upc = input(
+        "\nThat input is invalid. \nPlease enter a 12 numerical digit UPC:\n")
 
 print(f"{user_upc} {type(user_upc)}")
