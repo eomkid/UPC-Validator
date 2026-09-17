ad56f29685f1e8bdec50b1dbb104d50c938fe997
Brandon Barrett <bbarrett148@gmail.com>
2026-09-17 01:48:01 -0400

    Correct calculation inside the find_UPC function

--- Lab1_eomkid-1.py
+++ Lab1_eomkid-1.py
@@ -25,10 +25,12 @@ def find_UPC(upc_to_validate, validation_digit: int = upc_validation_digit):
     odd_positioned_tripled = odd_positioned_upc_digits[:]
 
     odd_positioned_tripled = [digit * 3 for digit in odd_positioned_tripled]
-    validation_check = sum(odd_positioned_tripled +
-                           even_positioned_upc_digits) % validation_digit
-
-    print(f"{validation_check}")
+    validation_digit_check = sum(odd_positioned_tripled +
+                                 even_positioned_upc_digits) % 10
+    if validation_digit_check == validation_digit or 10 - validation_digit_check == validation_digit:
+        print(f"Valid UPC")
+    else:
+        print("Invalid")
     return
 
 
